e77ec43f86da03a1cb5a1a5a4a45d6563563429f
Allen <1109551489>
2022-10-27 12:13:52 +0800

    添加 Dockerfile

--- jenkins-docker-demo/service_start.sh
+++ jenkins-docker-demo/service_start.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-java  -server -Dserver.port=80 -jar /app/jenkins-docker-demo.jar --spring.profiles.active=dev
+java -Dserver.port=80 -jar /app/jenkins-docker-demo.jar --spring.profiles.active=dev
